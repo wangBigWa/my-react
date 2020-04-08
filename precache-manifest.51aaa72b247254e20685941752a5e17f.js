0ee5e021f2fd72c036ab8a81bd015a65702efc48
DESKTOP-D6PBT3D\SunnySong <752642526@qq.com>
2020-04-08 16:29:59 +0800

    deploy

---
+++ precache-manifest.51aaa72b247254e20685941752a5e17f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1202c7901cab9723cde62e1c8ee12b95",
+    "url": "/my-react/index.html"
+  },
+  {
+    "revision": "2bb89f4b1d1cd6a34761",
+    "url": "/my-react/static/css/main.06d85342.chunk.css"
+  },
+  {
+    "revision": "8d56e004d541bcd8c98f",
+    "url": "/my-react/static/js/2.109d7264.chunk.js"
+  },
+  {
+    "revision": "2bb89f4b1d1cd6a34761",
+    "url": "/my-react/static/js/main.92ce8a52.chunk.js"
+  },
+  {
+    "revision": "cc9278b06e1425773c6c",
+    "url": "/my-react/static/js/runtime~main.9d651b49.js"
+  }
+]);
